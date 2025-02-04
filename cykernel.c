4060ba7b9323d408e80ae77da0d0e251ebae02c7
Haru-93 <hal-ishikawa2046@outlook.jp>
2025-02-04 21:19:08 +0900

    簡易マルチタスクの追加

--- cykernel.c
+++ cykernel.c
@@ -1,32 +1,63 @@
 #include <stdio.h>
 #include <setjmp.h>
+#include <unistd.h>
 
-jmp_buf context;
+#define MAX_TASK 2
+#define TASK_STACK_SIZE 1024
 
-void func_2(void)
+void task_1(void);
+void task_2(void);
+typedef void (*FP)(void);
+FP fp_table[] = {
+    task_1,
+    task_2,
+};
+
+jmp_buf context_table[MAX_TASK];
+
+#define DISPATCH(from, to)((setjmp(from)==0)?longjmp(to, 1):0)
+
+void task_1(void)
 {
-    printf("start func_2\n");
-    longjmp(context, 1);
-    printf("end func2\n");
+    while(1) {
+        printf("task 1\n");
+        sleep(1);
+        DISPATCH(context_table[0], context_table[1]);
+    }
 }
 
-void func_1(void)
+void task_2(void)
 {
-    printf("start func_1\n");
-    func_2();
-    printf("end func1\n");
+    while(1) {
+        printf("task 2\n");
+        sleep(1);
+        DISPATCH(context_table[1], context_table[0]);
+    }
+}
+
+void make_stack(int n);
+
+void init_context(int n)
+{
+    if(n < MAX_TASK) {
+        if(setjmp(context_table[n]) == 0) {
+            make_stack(n);
+        }
+        else {
+            fp_table[n]();
+        }
+    }
+}
+
+void make_stack(int n)
+{
+    char stack[TASK_STACK_SIZE];
+    init_context(++n);
 }
 
 int main()
 {
-    printf("start main\n");
-
-    if(setjmp(context) == 0)
-    {
-        func_1();
-    }
-
-    printf("end main\n");
-
+    make_stack(-1);
+    longjmp(context_table[0],1);
     return 0;
 }
