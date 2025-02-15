488260c9a0b791057bf161a10bced22d7d5ffff9
Haru-93 <hal-ishikawa2046@outlook.jp>
2025-02-15 13:28:08 +0900

    タスク起床

--- appli.c
+++ appli.c
@@ -3,12 +3,15 @@
 #include <setjmp.h>
 #include "cykernel.h"
 
+ID taskid_1, taskid_2;
+
 void task_1(void)
 {
     while(1) {
         printf("task_1\n");
         cy_delay_task(500);
-        scheduler();
+        cy_wakeup_task(taskid_2);
+        cy_sleep_task(TIMEOUT_FOREVER);
     }
 }
 
@@ -17,55 +20,28 @@ void task_2(void)
     while(1) {
         printf("task_2\n");
         cy_delay_task(500);
-        scheduler();
-    }
-}
-
-void task_3(void)
-{
-    while(1) {
-        printf("task_3\n");
-        cy_delay_task(500);
-        scheduler();
-    }
-}
-
-void task_4(void)
-{
-    while(1) {
-        printf("task_4\n");
-        cy_delay_task(500);
-        scheduler();
+        cy_wakeup_task(taskid_1);
+        cy_sleep_task(TIMEOUT_FOREVER);
     }
 }
 
 Type_Create_Task createtask_1 = {
     .task = task_1,
-    .task_priority = 3
+    .task_priority = 1
 };
 Type_Create_Task createtask_2 = {
     .task = task_2,
     .task_priority = 2
 };
-Type_Create_Task createtask_3 = {
-    .task = task_3,
-    .task_priority = 3
-};
-Type_Create_Task createtask_4 = {
-    .task = task_4,
-    .task_priority = 4
-};
+
 
 void usermain(void)
 {
-    cy_create_task(&createtask_1);
-    cy_create_task(&createtask_2);
-    cy_create_task(&createtask_3);
-    cy_create_task(&createtask_4);
+    taskid_1 = cy_create_task(&createtask_1);
+    taskid_2 = cy_create_task(&createtask_2);
     
     while(1){
         printf("usermain\n");
-        cy_delay_task(500);
-        scheduler();
+		cy_sleep_task(TIMEOUT_FOREVER);
     }
 }
